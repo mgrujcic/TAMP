647474705cf6d02c5300e6f1e470f430522cfff1
mgrujcic <markogrujcic@hotmail.com>
2023-02-25 23:53:16 +0100

    MapReduce word count

--- Chapter17/src/MapReduce.java
+++ Chapter17/src/MapReduce.java
@@ -1,3 +1,64 @@
-public class MapReduce {
-    
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ForkJoinPool;
+import java.util.function.Supplier;
+
+public class MapReduce<IN, K, V, OUT> implements Callable<Map<K, OUT>>  {
+    private List<IN> inputList;
+    private Supplier<Mapper<IN, K, V>> mapperSupplier;
+    private Supplier<Reducer<K, V, OUT>> reducerSupplier;
+    private static ForkJoinPool pool;
+
+    public MapReduce(){
+        pool = new ForkJoinPool();
+        mapperSupplier = () -> {throw new UnsupportedOperationException("no mapper supplier");};
+        reducerSupplier = () -> {throw new UnsupportedOperationException("no reducer supplier");};
+    }
+    public Map<K, OUT> call(){
+        Set<Mapper<IN, K, V>> mappers = new HashSet<>();
+        for(IN input: inputList){
+            Mapper<IN, K, V> mapper = mapperSupplier.get();
+            mapper.setInput(input);
+            pool.execute(mapper);
+            mappers.add(mapper);
+        }
+        Map<K, List<V>> mapResults = new HashMap<>();
+        for(Mapper<IN, K, V> mapper: mappers){
+            Map<K, V> map = mapper.join();
+            for(K key: map.keySet()){
+                mapResults.putIfAbsent(key, new LinkedList<>());
+                mapResults.get(key).add(map.get(key));
+            }
+        }
+        Map<K, Reducer<K, V, OUT>> reducers = new HashMap<>();
+        mapResults.forEach(
+            (k, v) -> {
+                Reducer<K, V, OUT> reducer = reducerSupplier.get();
+                reducer.setInput(k, v);
+                pool.execute(reducer);
+                reducers.put(k, reducer);
+            }
+        );
+        Map<K, OUT> result = new HashMap<>();//
+        reducers.forEach(
+            (k, reducer) -> {
+                result.put(k, reducer.join());
+            }
+        );
+        return result;
+    }
+    public void setMapperSupplier(Supplier<Mapper<IN, K, V>> mapperSupplier){
+        this.mapperSupplier = mapperSupplier;
+    }
+    public void setReducerSupplpier(Supplier<Reducer<K, V, OUT>> reducerSupplier){
+        this.reducerSupplier = reducerSupplier;
+    }
+    public void setInput(List<IN> inputList){
+        this.inputList = inputList;
+    }
 }
---
+++ Chapter17/src/WordCount.java
@@ -0,0 +1,84 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+
+public class WordCount {
+    static List<String> text;
+    static int numThreads = 8;
+    
+    public static void main(String[] args) {
+        text = readFile("src/revalation.txt");
+        List<List<String>> inputs = splitInputs(text);
+        MapReduce<List<String>, String, Long, Long> mapReduce = new MapReduce<>();
+        mapReduce.setMapperSupplier(WordCount.WCMapper::new);
+        mapReduce.setReducerSupplpier(WordCount.WCReducer::new);
+        mapReduce.setInput(inputs);
+        Map<String, Long> map = mapReduce.call();
+        displayOutout(map);
+    }
+
+    private static void displayOutout(Map<String, Long> map) {
+        map.entrySet().stream().sorted(
+            (x1, x2) -> {return Long.compare(x2.getValue(), x1.getValue());}
+        ).limit(10)
+         .forEach((kvp) -> {System.out.println(kvp.getKey() + ": " + kvp.getValue());});
+    }
+
+    private static List<List<String>> splitInputs(List<String> words) {
+        ArrayList<List<String>> divided = new ArrayList<>(numThreads);
+        for(int i = 0; i < numThreads; i++) 
+            divided.add(new LinkedList<>());
+        int nextList = 0;
+        for(var word: words){
+            divided.get(nextList).add(word);
+            nextList = (nextList + 1) % numThreads;
+        }
+        return divided;
+    }
+
+    private static List<String> readFile(String file) {
+        List<String> words = new LinkedList<>();
+        try(Scanner sc = new Scanner(Files.newInputStream(Path.of(file)))){
+
+            while(sc.hasNext()){
+                sc.useDelimiter("\\b");
+                String word = sc.next();
+                if(word.chars().allMatch(Character::isAlphabetic))
+                    words.add(word.toLowerCase());
+            }
+        }catch(IOException ex){
+            ex.printStackTrace();
+            System.exit(-1);
+        }
+        return words;
+    }
+
+    static private class WCMapper extends Mapper<List<String>, String, Long> {
+        @Override
+        protected Map<String, Long> compute() {
+            Map<String, Long> map = new HashMap<>();
+            input.forEach(
+                (w) -> map.merge(w, 1L, (x, y) -> x + y)
+            );
+            return map;
+        }
+    }
+
+    static private class WCReducer extends Reducer<String, Long, Long>{
+        @Override
+        protected Long compute() {
+            long count = 0;
+            for(long c: valueList){
+                count += c;
+            }
+            return count;
+        }
+    }
+}
